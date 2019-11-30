c8b63281da55c343ad6ea86a3bce3e337cf83583
wuyuxin <876465382@qq.com>
2019-11-30 13:52:19 +0800

    fix:调整程序入口，从命令行启动时不显示GUI

--- main.cpp
+++ main.cpp
@@ -6,12 +6,14 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
     if(argc==3){  //接收到命令行参数
         QString instr_file(argv[1]);
         QString dir_path(argv[2]);
         w.DrawFromInstruction(instr_file,dir_path);
-
+        a.exit();
+    }
+    else{
+        w.show();
     }
 
     return a.exec();
